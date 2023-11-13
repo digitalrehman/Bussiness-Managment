0267734e2b6240ed5c9e1bb0b29a0d5b2ec5e24e
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-13 23:55:03 +0500

    Update Karachi.jsx

--- src/Componentes/Karachi/Karachi.jsx
+++ src/Componentes/Karachi/Karachi.jsx
@@ -6,6 +6,9 @@ const Karachi = () => {
     <>
       <div className="container">
         <div className="row m-4">
+          <div className="Text-center">
+          <h1 className="text-light fw-bold">Karachi</h1>
+          </div>
           <div className="col-12">
             <Link to={"/NovKarLoop"} className="btn btn-dark btn-lg m-1">
               November
