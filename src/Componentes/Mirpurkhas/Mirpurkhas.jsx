422cf1d28e360d934b4ecb0fa742b4305bc199b1
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-14 00:03:29 +0500

    Update Mirpurkhas.jsx

--- src/Componentes/Mirpurkhas/Mirpurkhas.jsx
+++ src/Componentes/Mirpurkhas/Mirpurkhas.jsx
@@ -7,7 +7,7 @@ const Mirpurkhas = () => {
       <div className="container">
         <div className="row m-4">
           <div className="text-center">
-          <h1 className="text-light fw-bold">Mirpurkhas</>
+          <h1 className="text-light fw-bold">Mirpurkhas</h1>
           </div>
           <div className="col-12">
             <Link to={"/NovLoop"} className="btn btn-dark btn-lg m-1">
