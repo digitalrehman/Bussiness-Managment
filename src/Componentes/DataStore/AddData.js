ca15ea47c00098a0dbd589bf769aa4c205ca3aa0
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-15 21:52:08 +0500

    Update AddData.js

--- src/Componentes/DataStore/AddData.js
+++ src/Componentes/DataStore/AddData.js
@@ -2,22 +2,27 @@ let Add_Data = [
   {
     date: "11/11/2023",
     sale: "18030",
-    profit: "11845",
+    profit: "9045",
   },
    {
     date: "12/11/2023",
     sale: "14460",
-    profit: "9360",
+    profit: "7610",
   },
    {
     date: "13/11/2023",
     sale: "11520",
-    profit: "8120",
+    profit: "4470",
   },
    {
     date: "14/11/2023",
     sale: "9540",
-    profit: "5940",
+    profit: "4420",
+  },
+    {
+    date: "15/11/2023",
+    sale: "8270",
+    profit: "3090",
   }
 ];
 export { Add_Data };
