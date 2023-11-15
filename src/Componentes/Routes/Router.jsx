aed8b1940cc6908bd47ffc08ae347a399088bc21
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-15 15:28:02 +0500

    Update Router.jsx

--- src/Componentes/Routes/Router.jsx
+++ src/Componentes/Routes/Router.jsx
@@ -6,6 +6,19 @@ import Karachi from '../Karachi/Karachi'
 import NovKarLoop from '../Karachi Data Looping/NovKarLoop'
 import NovLoop from '../Mirpurkhas Data Looping/NovLoop'
 import Collections from '../Collections/Collections'
+import Debit from '../Debit Details/Debit'
+import Afzal_Hussain from '../Debit Details/Afzal_Hussain'
+import Akbar_Adil from '../Debit Details/Akbar_Adil'
+import Anwar_Adil from '../Debit Details/Anwar_Adil'
+import Ayaz_Adil from '../Debit Details/Ayaz_Adil'
+import Eram from '../Debit Details/Eram'
+import Faqeer_M_Saleem from '../Debit Details/Faqeer_M_Saleem'
+import Ghazala from '../Debit Details/Ghazala'
+import Rasool_Bakhsh from '../Debit Details/Rasool_Bakhsh'
+import Raziya from '../Debit Details/Raziya'
+import Rukhsana from '../Debit Details/Rukhsana'
+import Sana from '../Debit Details/Sana'
+import Summra from '../Debit Details/Summra'
 
 const Router = () => {
   return (
@@ -17,6 +30,19 @@ const Router = () => {
         <Route path='/NovLoop' element={<NovLoop/>}/>
         <Route path='/NovKarLoop' element={<NovKarLoop/>}/>
         <Route path='/Collections' element={<Collections/>}/>
+        <Route path='/Debit' element={<Debit/>}/>
+        <Route path='/Afzal_Hussain' element={<Afzal_Hussain/>}/>
+        <Route path='/Akbar_Adil' element={<Akbar_Adil/>}/>
+        <Route path='/Anwar_Adil' element={<Anwar_Adil/>}/>
+        <Route path='/Ayaz_Adil' element={<Ayaz_Adil/>}/>
+        <Route path='/Eram' element={<Eram/>}/>
+        <Route path='/Faqeer_M_Saleem' element={<Faqeer_M_Saleem/>}/>
+        <Route path='/Ghazala' element={<Ghazala/>}/>
+        <Route path='/Rasool_Bakhsh' element={<Rasool_Bakhsh/>}/>
+        <Route path='/Raziya' element={<Raziya/>}/>
+        <Route path='/Rukhsana' element={<Rukhsana/>}/>
+        <Route path='/Sana' element={<Sana/>}/>
+        <Route path='/Summra' element={<Summra/>}/>
         
       </Routes>
     </>
