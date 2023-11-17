bebe1f6ce5ec771ff5c2404347c3aa5bc6e81a0d
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-17 22:34:17 +0500

    Update Collections.jsx

--- src/Componentes/Collections/Collections.jsx
+++ src/Componentes/Collections/Collections.jsx
@@ -8,35 +8,7 @@ const Collections = () => {
     <div className="container">
         <div className="row my-3">
             <div className="col-12">
-                <div className="text-center">
-                    <h1 className='fw-bold text-light'>Total Bussiness Detail</h1>
-                </div>
-        
-      <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Bussiness Detail</th>
-          <th>Mirpurkhas</th>
-          <th>Karachi</th>
-    
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Total Bussiness</th>
-          <td>140860</td>
-          <td>53550</td>
-      
-        </tr>
-        <tr>
-          <th>Profit</th>
-          <td>35400</td>
-          <td>35265</td>
-  
-        </tr>
-      </tbody>
-    </Table>
-
+              
               <div className="text-center my-3">
               <h1 className="fw-bold text-light">Monthly Reports</h1>
               </div>
