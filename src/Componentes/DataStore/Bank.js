ccb9c4771937ae09c7eea493e9a64113b68cbf8a
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-20 16:21:44 +0500

    Update Bank.js

--- src/Componentes/DataStore/Bank.js
+++ src/Componentes/DataStore/Bank.js
@@ -1,32 +1,37 @@
 let Bank = [
-    {
-      date: "00",
-      name: "00",
-      credit: "00",
-      debit_mir: "00",
-      debit_kar: "00"
-    },
-    
-  ];
-  
-  
-  let sum = 0;
-  for (let i = 0; i < Bank.length; i++) {
-    sum += parseInt(Bank[i].credit);
-  }
-  let summir = 0;
+  {
+    date: "00",
+    name: "00",
+    credit_mir: "00",
+    credit_kar: "00",
+    debit_mir: "00",
+    debit_kar: "00",
+  },
+];
+
+let summ = 0;
+for (let i = 0; i < Bank.length; i++) {
+  summ += parseInt(Bank[i].credit_mir);
+}
+let sumk = 0;
+for (let i = 0; i < Bank.length; i++) {
+  sumk += parseInt(Bank[i].credit_kar);
+}
+let summir = 0;
 for (let i = 0; i < Bank.length; i++) {
   summir += parseInt(Bank[i].debit_mir);
-} let sumkar = 0;
+}
+let sumkar = 0;
 for (let i = 0; i < Bank.length; i++) {
   sumkar += parseInt(Bank[i].debit_kar);
 }
-  let sum_Ans = [
-    {
-      Sumvalue : sum,
-      Sumprofit : summir,
-      Sumkar : sumkar
-    },
-  ]
+let sum_Ans = [
+  {
+    Summir: summ,
+    Sumk: sumk,
+    Sumprofit: summir,
+    Sumkar: sumkar,
+  },
+];
 
-  export { Bank, sum_Ans };
+export { Bank, sum_Ans };
