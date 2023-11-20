2ef720eb4501fcb34bd87654e9bd3a0a38e42718
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-20 15:54:55 +0500

    Update Raziya.jsx

--- src/Componentes/Debit Details/Raziya.jsx
+++ src/Componentes/Debit Details/Raziya.jsx
@@ -15,8 +15,8 @@ const Raziya = () => {
                 <tr>
                   <th scope="col">Date</th>
                   <th scope="col">Description</th>
-                  <th scope="col">Debit</th>
                   <th scope="col">Credit</th>
+                  <th scope="col">Debit</th>
                 </tr>
               </thead>
               <tbody>
@@ -26,18 +26,7 @@ const Raziya = () => {
                   <td>375000</td>
                   <td>0</td>
                 </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
+                
               </tbody>
             </table>
           </div>
