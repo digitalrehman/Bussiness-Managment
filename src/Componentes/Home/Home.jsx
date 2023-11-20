aca517eb1e5d25c81b970171b3822af90e8de143
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-20 15:42:52 +0500

    Update Home.jsx

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
               to="/Collections"
               className="text-light text-decoration-none p-2 bg-dark rounded-2 m-1"
             >
-              Monthly Reports
+              Business
             </Link>
             <Link
               to="/Bank_Detail"
