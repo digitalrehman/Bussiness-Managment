612bac5e4a19bc63e33bd22d7c27e285c80e5454
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-22 14:20:41 +0500

    Add files via upload

---
+++ src/Componentes/Debit Details/Shoaib_Akhtar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Shoaib_Akhtar = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row my-3">
+          <div className="text-center">
+            <h1 className="fw-bold text-light">Shoaib Akhtar</h1>
+            <p className="text-light fs-5">Debit Information</p>
+          </div>
+          <div className="col-12">
+            <table className="table table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">Date</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Credit</th>
+                  <th scope="col">Debit</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>0</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Shoaib_Akhtar
