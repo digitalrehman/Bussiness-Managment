88669a0327b93986fce8c4601d1e8990b5d34f8c
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-11-22 14:36:50 +0500

    Update Debit.jsx

--- src/Componentes/Debit Details/Debit.jsx
+++ src/Componentes/Debit Details/Debit.jsx
@@ -51,7 +51,7 @@ const Debit = () => {
             Shoaib Akhtar
             </Link>
             <Link to={"/Irfan_Chandaji"} className="btn btn-dark btn-lg m-1">
-            Shoaib Akhtar
+            Irfan Chandaji
             </Link>
           </div>
         </div>
