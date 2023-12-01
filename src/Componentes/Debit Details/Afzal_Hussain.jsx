9722399a37ca004388ce4784db431fe3d831946c
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-01 16:40:07 +0500

    Update Afzal_Hussain.jsx

--- src/Componentes/Debit Details/Afzal_Hussain.jsx
+++ src/Componentes/Debit Details/Afzal_Hussain.jsx
@@ -1,49 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import page3 from "../../assets/IMG/afzal.jpeg";
+import { Table } from "react-bootstrap";
 
 const Afzal_Hussain = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+  const data = [
+    {
+      page: "1",
+      Date: "5/11/2021",
+      description: "2 saal ke hisaab walay",
+      credit: "350000",
+      debit: "00",
+      balance: "350000",
+    },
+    {
+      page: "1",
+      Date: "00",
+      description: "Hajj Walay Bakaya",
+      credit: "650000",
+      debit: "00",
+      balance: "1000000",
+    },
+  ];
+
   return (
-    <>
-      <div className="container">
-        <div className="row my-3">
-          <div className="text-center">
-            <h1 className="fw-bold text-light">Afzal Hussain</h1>
-            <p className="text-light fs-5">Debit Information</p>
+    <div>
+      <h1
+        style={{
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "13px",
+          marginBottom: "10px",
+          color: "snow",
+        }}
+      >
+        Afzal Hussain
+      </h1>
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        variant="dark"
+        style={{ overflowX: "auto" }}
+      >
+        <thead>
+          <tr>
+            <th style={{ color: "aqua", textAlign: "center" }}>Page</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Date</th>
+            <th style={{ width: "300px", color: "aqua", textAlign: "center" }}>
+              Description
+            </th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Credit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Debit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td style={{ textAlign: "center" }}>{item.page}</td>
+              <td style={{ textAlign: "center" }}>{item.Date}</td>
+              <td>{item.description}</td>
+              <td style={{ textAlign: "center" }}>{item.credit}</td>
+              <td style={{ textAlign: "center" }}>{item.debit}</td>
+              <td
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {item.balance}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div>
+        <button onClick={handleClick} className="btn btn-dark fw-bold my-3">
+          {!visible ? "Page 1" : "Close"}
+        </button>
+        {visible && (
+          <div
+            style={{
+              width: "95vw",
+              height: "100vh",
+            }}
+          >
+            <img src={page3} style={{ height: "auto", width: "100%" }} />
           </div>
-          <div className="col-12">
-            <table className="table table-dark">
-              <thead>
-                <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Credit</th>
-                  <th scope="col">Debit</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
