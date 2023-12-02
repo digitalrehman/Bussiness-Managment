bcce02f850976c0782bf428f879c0129d9065021
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-02 08:36:22 +0500

    Update Collections.jsx

--- src/Componentes/Collections/Collections.jsx
+++ src/Componentes/Collections/Collections.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import Table from 'react-bootstrap/Table';
-
+import React from "react";
 
 const Collections = () => {
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="row my-3">
-            <div className="col-12">
-              
-              <div className="text-center my-3">
+          <div className="col-12">
+            <div className="text-center my-3">
               <h1 className="fw-bold text-light">Monthly Reports</h1>
-              </div>
-              <table className="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col" className="text-primary">Month</th>
-      <th scope="col" className="text-primary">Sale</th>
-      <th scope="col" className="text-primary">Profit</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">November</th>
-      <td>259290</td>
-      <td>115325</td>
-  
-    </tr>
-      <tr>
-      <th scope="row">December</th>
-      <td>0</td>
-      <td>0</td>
-  
-    </tr>
-  </tbody>
-</table>
-              <div className="col-10 text-center">
+            </div>
+            <table className="table table-dark">
+              <thead>
+                <tr>
+                  <th scope="col" className="text-primary">
+                    City
+                  </th>
+                  <th scope="col" className="text-primary">
+                    Month
+                  </th>
+                  <th scope="col" className="text-primary">
+                    Sale
+                  </th>
+                  <th scope="col" className="text-primary">
+                    Profit
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Karachi</th>
+                  <th scope="row">November</th>
+                  <td>259290</td>
+                  <td>115325</td>
+                </tr>
+                <tr>
+                  <th scope="row">Mirpurkhas</th>
+                  <th scope="row">November</th>
+                  <td>918690</td>
+                  <td>213200</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="col-10 text-center">
               <div>
                 <h1 className="fw-bold text-light">Debit Details</h1>
               </div>
@@ -48,17 +54,17 @@ const Collections = () => {
                 </thead>
                 <tbody>
                   <tr>
-                    <th>5675000</th>
+                    <th>0</th>
                     <td>0</td>
                   </tr>
                 </tbody>
               </table>
             </div>
-    </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
