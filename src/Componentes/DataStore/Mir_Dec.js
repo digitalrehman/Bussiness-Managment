f2b839bada8400dca65741598ab2e301dfcc32ee
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-02 08:22:47 +0500

    Add files via upload

---
+++ src/Componentes/DataStore/Mir_Dec.js
@@ -0,0 +1,25 @@
+let Add_Data_Dec = [
+    {
+      date: "1/12/2023",
+      sale: "00",
+      profit: "00",
+    }
+  ];
+  
+  let sum = 0;
+  for (let i = 0; i < Add_Data_Dec.length; i++) {
+    sum += parseInt(Add_Data_Dec[i].sale);
+  }
+  let sumPro = 0;
+  for (let i = 0; i < Add_Data_Dec.length; i++) {
+    sumPro += parseInt(Add_Data_Dec[i].profit);
+  }
+  
+  let sum_Ans = [
+    {
+      Sumvalue: sum,
+      Sumprofit: sumPro,
+    },
+  ];
+  export { Add_Data_Dec, sum_Ans };
+  
