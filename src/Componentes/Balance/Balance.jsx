566deca425f1d881963ffc5b57fac53cfca2b7e3
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-02 14:45:27 +0500

    Update Balance.jsx\

--- src/Componentes/Balance/Balance.jsx
+++ src/Componentes/Balance/Balance.jsx
@@ -1,6 +1,94 @@
 import React from "react";
-
+import { Table } from "react-bootstrap";
 const Balance = () => {
+  const data = [
+    {
+      page: "38",
+      Name: "Imran Bhai Floor",
+      balance: "80000",
+    },
+    {
+      page: "39",
+      Name: "Abdullah",
+      balance: "200000",
+    },
+    {
+      page: "40",
+      Name: "kamran Beg",
+      balance: "500000",
+    },
+    {
+      page: "37",
+      Name: "Younus Hyderabad",
+      balance: "500000",
+    },
+    {
+      page: "36",
+      Name: "Shahzad Bhai",
+      balance: "600000",
+    },
+    {
+      page: "29",
+      Name: "Rasool Bakhsh",
+      balance: "900000",
+    },
+    {
+      page: "33",
+      Name: "Irfaan Chandija",
+      balance: "3100000",
+    },
+    {
+      page: "27",
+      Name: "Shoaib Akhtar",
+      balance: "2500000",
+    },
+    {
+      page: "26",
+      Name: "M Saleem",
+      balance: "1275000",
+    },
+    {
+      page: "23",
+      Name: "Eram",
+      balance: "53000",
+    },
+    {
+      page: "1",
+      Name: "Anwar Adil",
+      balance: "1939000",
+    },
+    {
+      page: "15",
+      Name: "Afzal Hussain",
+      balance: "2250000",
+    },
+    {
+      page: "25",
+      Name: "Samra",
+      balance: "100000",
+    },
+    {
+      page: "24",
+      Name: "Sana",
+      balance: "375000",
+    },
+    {
+      page: "22",
+      Name: "Ghazala",
+      balance: "375000",
+    },
+    {
+      page: "11",
+      Name: "Ayaz",
+      balance: "1190000",
+    },
+  ];
+
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += parseInt(data[i].balance);
+  }
+
   return (
     <>
       <div className="container">
@@ -9,32 +97,41 @@ const Balance = () => {
             <div className="text-center my-3 ">
               <h1 className="fw-bold">Balance</h1>
             </div>
-            <table class="table table-dark">
+            <Table
+              striped
+              bordered
+              hover
+              responsive
+              variant="dark"
+              style={{ overflowX: "auto" }}
+            >
               <thead>
                 <tr>
-                  <th scope="col">Page</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Balance</th>
+                  <th style={{ textAlign: "center" }}>Page</th>
+                  <th style={{ textAlign: "center" }}>Name</th>
+                  <th style={{ textAlign: "center" }}>Balance</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                  <td>@twitter</td>
-                </tr>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td style={{ textAlign: "center" }}>{item.page}</td>
+                    <td style={{ textAlign: "center" }}>{item.Name}</td>
+                    <td style={{ textAlign: "center" }}>{item.balance}</td>
+                  </tr>
+                ))}
               </tbody>
-            </table>
+              <tfoot>
+                <tr>
+                  <td
+                    colSpan="3"
+                    style={{ textAlign: "right", fontWeight: "bold" }}
+                  >
+                    Total Balance : {sum}
+                  </td>
+                </tr>
+              </tfoot>
+            </Table>
           </div>
         </div>
       </div>
