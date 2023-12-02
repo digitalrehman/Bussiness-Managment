0f2b4c61af68803fbd10ea37aed3768ce53a468d
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-02 08:53:48 +0500

    Update Irfan_Chandaji.jsx

--- src/Componentes/Debit Details/Irfan_Chandaji.jsx
+++ src/Componentes/Debit Details/Irfan_Chandaji.jsx
@@ -1,38 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import page2 from "../../assets/IMG/irfaan.jpeg";
+import { Table } from "react-bootstrap";
 
 const Irfan_Chandaji = () => {
-  return (
-    <>
-        <div className="container">
-        <div className="row my-3">
-          <div className="text-center">
-            <h1 className="fw-bold text-light">Irfan Chandaji</h1>
-            <p className="text-light fs-5">Debit Information</p>
-          </div>
-          <div className="col-12">
-            <table className="table table-dark">
-              <thead>
-                <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Credit</th>
-                  <th scope="col">Debit</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
+  const [visible, setVisible] = useState(false);
 
-export default Irfan_Chandaji
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+  const data = [
+    {
+      page: "33",
+      Date: "05/11/2023",
+      description: "Changan Walay",
+      credit: "240000",
+      debit: "00",
+      balance: "240000",
+    },
+    {
+      page: "33",
+      Date: "00",
+      description: "Highroof Walay",
+      credit: "70000",
+      debit: "00",
+      balance: "310000",
+    },
+  ];
+
+  return (
+    <div>
+      <h1
+        style={{
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "13px",
+          marginBottom: "10px",
+          color: "snow",
+        }}
+      >
+        Irfan Chandija
+      </h1>
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        variant="dark"
+        style={{ overflowX: "auto" }}
+      >
+        <thead>
+          <tr>
+            <th style={{ color: "aqua", textAlign: "center" }}>Page</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Date</th>
+            <th style={{ width: "300px", color: "aqua", textAlign: "center" }}>
+              Description
+            </th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Credit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Debit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td style={{ textAlign: "center" }}>{item.page}</td>
+              <td style={{ textAlign: "center" }}>{item.Date}</td>
+              <td>{item.description}</td>
+              <td style={{ textAlign: "center" }}>{item.credit}</td>
+              <td style={{ textAlign: "center" }}>{item.debit}</td>
+              <td
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {item.balance}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div>
+        <button onClick={handleClick} className="btn btn-dark fw-bold my-3">
+          {!visible ? "Page 33" : "Close"}
+        </button>
+        {visible && (
+          <div
+            style={{
+              width: "95vw",
+              height: "100vh",
+            }}
+          >
+            <img src={page2} style={{ height: "auto", width: "100%" }} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Irfan_Chandaji;
