801069d61a725f07ee3a4e68d00f2cdfe4821357
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-03 08:03:14 +0500

    Add files via upload

---
+++ src/Componentes/Debit Details/Kareem.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import page2 from "../../assets/IMG/kareem baksh.jpeg";
+import { Table } from "react-bootstrap";
+
+const Kareem = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+  const data = [
+    {
+      page: "35",
+      Date: "05/11/23",
+      description: "Car ke bakaya",
+      credit: "00",
+      debit: "800000",
+      balance: "800000",
+    },
+  ];
+
+  return (
+    <div>
+      <h1
+        style={{
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "13px",
+          marginBottom: "10px",
+          color: "snow",
+        }}
+      >
+        Kareem Bakhsh
+      </h1>
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        variant="dark"
+        style={{ overflowX: "auto" }}
+      >
+        <thead>
+          <tr>
+            <th style={{ color: "aqua", textAlign: "center" }}>Page</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Date</th>
+            <th style={{ width: "300px", color: "aqua", textAlign: "center" }}>
+              Description
+            </th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Credit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Debit</th>
+            <th style={{ color: "aqua", textAlign: "center" }}>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td style={{ textAlign: "center" }}>{item.page}</td>
+              <td style={{ textAlign: "center" }}>{item.Date}</td>
+              <td>{item.description}</td>
+              <td style={{ textAlign: "center" }}>{item.credit}</td>
+              <td style={{ textAlign: "center" }}>{item.debit}</td>
+              <td
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {item.balance}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div>
+        <button onClick={handleClick} className="btn btn-dark fw-bold my-3">
+          {!visible ? "Page 35" : "Close"}
+        </button>
+        {visible && (
+          <div
+            style={{
+              width: "95vw",
+              height: "100vh",
+            }}
+          >
+            <img src={page2} style={{ height: "auto", width: "100%" }} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Kareem;
