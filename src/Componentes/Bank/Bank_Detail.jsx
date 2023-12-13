9f922a542c493e3f8610a13262d5659b6041c76e
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-13 15:21:34 +0500

    Update Bank_Detail.jsx

--- src/Componentes/Bank/Bank_Detail.jsx
+++ src/Componentes/Bank/Bank_Detail.jsx
@@ -1,87 +1,24 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
-import { sum_Ans, Bank } from "../DataStore/Bank";
-const Bank_Detail = () => {
-  let { Summir, Sumk, Sumprofit, Sumkar } = sum_Ans[0];
+
+import { Link } from "react-router-dom";
+const Bank_Details = () => {
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row my-3">
-          <div className="text-center">
-            <h1 className="fw-bold text-light">Bank Details</h1>
-          </div>
-          <div className="col-12">
-            <table className="table table-dark">
-              <thead>
-                <tr>
-                  <th scope="col" className="text-primary">
-                    Date
-                  </th>
-                  <th scope="col" className="text-primary">
-                    Name
-                  </th>
-                  <th scope="col" className="text-primary">
-                    CR Mir
-                  </th>
-                  <th scope="col" className="text-primary">
-                    CR Kar
-                  </th>
-                  <th scope="col" className="text-primary">
-                    DE Mir
-                  </th>
-                  <th scope="col" className="text-primary">
-                    DE Kar
-                  </th>
-                </tr>
-              </thead>
-              {Bank.map((data,_id) => (
-                <tbody key={_id}>
-                  <tr>
-                    <td>{data.date}</td>
-                    <td>{data.name}</td>
-                    <td>{data.credit_mir}</td>
-                    <td>{data.credit_kar}</td>
-                    <td>{data.debit_mir}</td>
-                    <td>{data.debit_kar}</td>
-                  </tr>
-                </tbody>
-              ))}
-            </table>
-            <div className="row">
-              <div className="col-12">
-                <table className="table table-dark table-hover text-center">
-                  <thead className="text-center">
-                    <tr>
-                      <th scope="col" className="fs-5 text-info">
-                        Total CR Mir
-                      </th>
-                      <th scope="col" className="fs-5 text-info">
-                        Total CR Kar
-                      </th>
-                      <th scope="col" className="fs-5 text-info">
-                        Total DE Mir
-                      </th>
-                      <th scope="col" className="fs-5 text-info">
-                        Total DE Kar
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td scope="row">{Summir}</td>
-                      <td scope="row">{Sumk}</td>
-                      <td scope="row">{Sumprofit}</td>
-                      <td scope="row">{Sumkar}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+    <div className="container">
+      <div className="row m-4">
+        <div className="text-center">
+          <h1 className="text-light fw-bold">Karachi</h1>
+        </div>
+        <div className="col-12">
+          <Link to="/Karachi_Expense" className="btn btn-dark btn-lg m-1">
+            Karachi 11/2023
+          </Link>
+          <Link to="/Mirpur_Expense" className="btn btn-dark btn-lg m-1">
+            Mirpurkhas 11/2023
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default Bank_Detail;
+export default Bank_Details;
