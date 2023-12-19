d1b671b6f338eca6ffd22a01cf40880b3668667b
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2023-12-19 08:08:38 +0500

    Update Balance.jsx

--- src/Componentes/Balance/Balance.jsx
+++ src/Componentes/Balance/Balance.jsx
@@ -3,95 +3,87 @@ import { Table } from "react-bootstrap";
 const Balance = () => {
   const data = [
     {
-      page: "38",
-      Name: "Imran Bhai Floor",
-      balance: "80000",
-    },
-    {
-      page: "39",
-      Name: "Abdullah",
-      balance: "200000",
-    },
-    {
-      page: "40",
-      Name: "kamran Beg",
-      balance: "500000",
-    },
-    {
-      page: "37",
-      Name: "Younus Hyderabad",
-      balance: "500000",
-    },
-    {
-      page: "36",
-      Name: "Shahzad Bhai",
-      balance: "600000",
-    },
-    {
-      page: "29",
-      Name: "Rasool Bakhsh",
-      balance: "900000",
-    },
-    {
-      page: "33",
-      Name: "Irfaan Chandija",
-      balance: "3100000",
-    },
-    {
-      page: "27",
-      Name: "Shoaib Akhtar",
-      balance: "2500000",
-    },
-    {
-      page: "26",
-      Name: "M Saleem",
-      balance: "1275000",
-    },
-    {
-      page: "23",
-      Name: "Eram",
-      balance: "53000",
-    },
-    {
       page: "1",
       Name: "Anwar Adil",
       balance: "1939000",
-    },
-    {
-      page: "15",
-      Name: "Afzal Hussain",
-      balance: "2250000",
-    },
-    {
-      page: "25",
-      Name: "Samra",
-      balance: "1000000",
-    },
-    {
-      page: "24",
-      Name: "Sana",
-      balance: "375000",
-    },
-    {
-      page: "22",
-      Name: "Ghazala",
-      balance: "375000",
-    },
-    {
-      page: "11",
-      Name: "Ayaz Adil",
-      balance: "1190000",
-    },
-    {
+    },{
       page: "5",
       Name: "Akbar Adil",
       balance: "-2601400",
-    },
-    {
+    },{
+      page: "11",
+      Name: "Ayaz Adil",
+      balance: "1190000",
+    },{
+      page: "15",
+      Name: "Afzal Hussain",
+      balance: "2250000",
+    },{
+      page: "22",
+      Name: "Ghazala",
+      balance: "375000",
+    },{
+      page: "23",
+      Name: "Eram",
+      balance: "53000",
+    },{
+      page: "24",
+      Name: "Sana",
+      balance: "375000",
+    },{
+      page: "25",
+      Name: "Samra",
+      balance: "1000000",
+    },{
+      page: "26",
+      Name: "M Saleem",
+      balance: "1275000",
+    },{
+      page: "27",
+      Name: "Shoaib Akhtar",
+      balance: "2500000",
+    },{
+      page: "29",
+      Name: "Rasool Bakhsh",
+      balance: "900000",
+    },{
+      page: "33",
+      Name: "Irfaan Chandija",
+      balance: "3100000",
+    },{
       page: "35",
       Name: "Kareem Bakhsh",
       balance: "-800000",
-    }
+    },{
+      page: "36",
+      Name: "Shahzad Bhai",
+      balance: "600000",
+    },{
+      page: "37",
+      Name: "Younus Hyderabad",
+      balance: "500000",
+    },{
+      page: "38",
+      Name: "Imran Bhai Floor",
+      balance: "80000",
+    },{
+      page: "39",
+      Name: "Abdullah",
+      balance: "200000",
+    },{
+      page: "40",
+      Name: "kamran Beg",
+      balance: "500000",
+    },{
+      page: "41",
+      Name: "Bushra Jameel",
+      balance: "129000",
+    },{
+      page: "42",
+      Name: "Munna Bhai",
+      balance: "100000",
+    },
+    
   ];
 
   let sum = 0;
