b21ef28b66aa2f19784869ac5f966d9a645047d1
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-02 08:50:09 +0500

    Update NovKarLoop.jsx

--- src/Componentes/Karachi Data Looping/NovKarLoop.jsx
+++ src/Componentes/Karachi Data Looping/NovKarLoop.jsx
@@ -1,49 +1,60 @@
 import React from "react";
 import { Add_Data, sum_Ans } from "../DataStore/AddData";
-import NovKarachi_2023 from "../Nov Karachi/NovKarachi_2023";
 
 const NovKarLoop = () => {
-    let { Sumvalue, Sumprofit, Akbar, Anwar } = sum_Ans[0];
+  let { Sumvalue, Sumprofit, Akbar, Anwar } = sum_Ans[0];
+  let salary = 67000;
+  let akbar_remaining = 67401;
+  let anwar_remaining = 67685;
   return (
     <>
-      <div className="container">
+      <div className="container-fluid">
         <div className="row my-3">
           <div className="col-12">
-          <div className="text-center">
-            <h1 className="fw-bold text-light">November Details</h1>
-          </div>
-          <table className="table table-dark">
-            <thead className="text-center">
-              <tr>
-                <th scope="col" className="fs-5 text-primary">
-                  Date
-                </th>
-                <th scope="col" className="fs-5 text-primary">
-                  Sale
-                </th>
-                <th scope="col" className="fs-5 text-primary">
-                  Profit
-                </th>
-                <th scope="col" className="fs-5 text-primary">
-                  Akbar
-                </th>
-                <th scope="col" className="fs-5 text-primary">
-                  Anwar
-                </th>
-              </tr>
-            </thead>
+            <div className="text-center">
+              <h1 className="fw-bold text-light">November Details</h1>
+            </div>
+            <table className="table table-dark">
+              <thead className="text-center">
+                <tr>
+                  <th scope="col" className="fs-5 text-primary">
+                    Date
+                  </th>
+                  <th scope="col" className="fs-5 text-primary">
+                    Sale
+                  </th>
+                  <th scope="col" className="fs-5 text-primary">
+                    Profit
+                  </th>
+                  <th scope="col" className="fs-5 text-primary">
+                    Akbar
+                  </th>
+                  <th scope="col" className="fs-5 text-primary">
+                    Anwar
+                  </th>
+                </tr>
+              </thead>
             </table>
             {Add_Data.map((data, index) => {
               return (
-                <div className="col-12" key={index}>
-                  <NovKarachi_2023 data={data} />
-                  
-                </div>
-                
+                <table
+                  className="table table-dark table-hover text-center"
+                  key={index}
+                >
+                  <tbody>
+                    <tr>
+                      <td scope="row">{data.date}</td>
+                      <td scope="row">{data.sale}</td>
+                      <td scope="row">{data.profit}</td>
+                      <td scope="row">{data.Akbar}</td>
+                      <td scope="row">{data.Anwar}</td>
+                    </tr>
+                  </tbody>
+                </table>
               );
             })}
-              <div className="row">
-              <div className="col-10">
+            <div className="row">
+              <div className="col-12">
                 <table className="table table-dark table-hover text-center">
                   <thead className="text-center">
                     <tr>
@@ -70,9 +81,82 @@ const NovKarLoop = () => {
                     </tr>
                   </tbody>
                 </table>
+
+                <table className="table table-dark table-hover text-center">
+                  <thead className="text-center">
+                    <tr>
+                      <th scope="col" className="fs-5 text-info">
+                        Total Salary
+                      </th>
+
+                      <th scope="col" className="fs-5 text-info">
+                        Akbar Credit
+                      </th>
+                      <th scope="col" className="fs-5 text-info">
+                        Anwar Credit
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td scope="row"> {salary} </td>
+                      <td scope="row"> {salary - Akbar} </td>
+                      <td scope="row"> {salary - Anwar} </td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <h3 className="text-center fw-bold my-4">Remaining Details</h3>
+                <table className="table table-dark table-hover text-center">
+                  <thead className="text-center">
+                    <tr>
+                      <th scope="col" className="fs-5 text-info">
+                        Name
+                      </th>
+
+                      <th scope="col" className="fs-5 text-info">
+                        Description
+                      </th>
+                      <th scope="col" className="fs-5 text-info">
+                        Akbar
+                      </th>
+                      <th scope="col" className="fs-5 text-info">
+                        Anwar
+                      </th>
+                      <th scope="col" className="fs-5 text-info">
+                        Total
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td scope="row" className="fw-bold text-info">
+                        {" "}
+                        Akbar
+                      </td>
+                      <td scope="row"> 11/11/23 previous </td>
+                      <td scope="row"> {akbar_remaining}</td>
+                      <td scope="row"> {anwar_remaining} </td>
+                      <td scope="row">
+                        {" "}
+                        {akbar_remaining + (salary - Akbar)}{" "}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td scope="row" className="fw-bold text-info">
+                        {" "}
+                        Anwar
+                      </td>
+                      <td scope="row"> 11/11/23 previous </td>
+                      <td scope="row"> {akbar_remaining} </td>
+                      <td scope="row"> {anwar_remaining}</td>
+                      <td scope="row">{anwar_remaining + (salary - Anwar)} </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
-        </div>
+          </div>
         </div>
       </div>
     </>
