0a6c4a2ebe05f8ea1942290497c994c9737a7421
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-05 10:05:12 +0500

    Update Collections.jsx

--- src/Componentes/Collections/Collections.jsx
+++ src/Componentes/Collections/Collections.jsx
@@ -39,26 +39,24 @@ const Collections = () => {
                   <td>918690</td>
                   <td>213200</td>
                 </tr>
+                <tr>
+                  <th scope="row">Karachi</th>
+                  <th scope="row">December</th>
+                  <td>399260</td>
+                  <td>179325</td>
+                </tr>
+                <tr>
+                  <th scope="row">Mirpurkhas</th>
+                  <th scope="row">December</th>
+                  <td>1595645</td>
+                  <td>316500</td>
+                </tr>
               </tbody>
             </table>
             <div className="col-10 text-center">
               <div>
                 <h1 className="fw-bold text-light">Debit Details</h1>
               </div>
-              <table className="table table-dark">
-                <thead>
-                  <tr>
-                    <th>Total Credit</th>
-                    <th>Total Debit</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>0</th>
-                    <td>0</td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
           </div>
         </div>
