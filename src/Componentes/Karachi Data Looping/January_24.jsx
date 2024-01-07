de905b3c274c293b4fb3419c7904f4bcbbd7a81a
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-07 11:11:51 +0500

    Update January_24.jsx

--- src/Componentes/Karachi Data Looping/January_24.jsx
+++ src/Componentes/Karachi Data Looping/January_24.jsx
@@ -133,7 +133,7 @@ const January_24 = () => {
                       <td scope="row" className="fw-bold text-info">
                         Akbar
                       </td>
-                      <td scope="row"> 11/23 previous </td>
+                      <td scope="row"> 12/23 previous </td>
                       <td scope="row"> {akbar_remaining}</td>
                       <td scope="row"> {anwar_remaining} </td>
                       <td scope="row">{akbar_remaining + (salary - Akbar)}</td>
@@ -142,7 +142,7 @@ const January_24 = () => {
                       <td scope="row" className="fw-bold text-info">
                         Anwar
                       </td>
-                      <td scope="row"> 11/23 previous </td>
+                      <td scope="row"> 12/23 previous </td>
                       <td scope="row"> {akbar_remaining} </td>
                       <td scope="row"> {anwar_remaining}</td>
                       <td scope="row">{anwar_remaining + (salary - Anwar)} </td>
