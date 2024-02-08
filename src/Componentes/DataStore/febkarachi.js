c7dc44ac317bb061765257930f93fb2514b01eb4
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-02-08 15:01:08 +0500

    Update febkarachi.js

--- src/Componentes/DataStore/febkarachi.js
+++ src/Componentes/DataStore/febkarachi.js
@@ -20,6 +20,34 @@ let Add_Data_Dec = [
       Akbar: "2790",
       Anwar: "2740",
     },
+    {
+      date: "4/2/2023",
+      sale: "12550",
+      profit: "7360",
+      Akbar: "2400",
+      Anwar: "1930",
+    },
+    {
+      date: "5/2/2023",
+      sale: "24960",
+      profit: "12030",
+      Akbar: "6290",
+      Anwar: "6290",
+    },
+    {
+      date: "6/2/2023",
+      sale: "13970",
+      profit: "6790",
+      Akbar: "5160",
+      Anwar: "5500",
+    },
+    {
+      date: "7/2/2023",
+      sale: "24050",
+      profit: "13720",
+      Akbar: "5200",
+      Anwar: "6480",
+    },
     
   ];
   
